d14046be4abf9e99f177f5abda7cded1e79e9252
rick1480 <rgonzalezj@gmail.com>
2018-11-30 13:48:53 -0500

    Change function name on /createreport to call the correct one

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,11 @@ var reportsCtrl = require('../controllers/reports.Ctrl');
 
 //creating routes for rendering home page
 router.get('/', function(req, res){
-  res.render('index'); 
+  res.render('index');
 });
 // creating routes for rendering createreport page and data for reporters and
 // projects drop-downs
-router.get('/createreport', reportsCtrl.allMembersReports, reportsCtrl.allProjects);
+// router.get('/createreport', reportsCtrl.allMembersReports, reportsCtrl.allProjects);
+router.get('/createreport', reportsCtrl.allMembersReportsAndProjects);
 //export router
 module.exports = router;
