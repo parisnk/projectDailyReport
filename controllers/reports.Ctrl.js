c464b96e6b914619995fd19d0b5eb1dd2ae575b7
rick1480 <rgonzalezj@gmail.com>
2018-11-30 13:47:19 -0500

    Create one function to fetch data

--- controllers/reports.Ctrl.js
+++ controllers/reports.Ctrl.js
@@ -4,6 +4,37 @@ var DailyReport = require('../models/DailyReport.model');
 var Reporter = require('../models/Reporter.model');
 var Project = require('../models/Project.model');
 
+exports.allMembersReportsAndProjects = function (req, res) {
+
+    //Find all the reporters
+    Reporter
+        .find({})
+        .sort({reporterName: 1})
+        .exec(function (err, reporters) {
+            // console.log(reporters);
+            if (err) {
+                console.log('error getting reporters');
+            } else {
+              Project
+                  .find({})
+                  .sort({ projectName: 1 })
+                  .exec(function (err, projects) {
+                      if (err) {
+                          console.log('error getting projects');
+                      } else {
+                          res.render('createReport', {
+                              title: 'Create Report',
+                              reporters: reporters,
+                              projects: projects
+                          });
+                      }
+                  });
+            }
+        });
+}
+
+// you can use this code to use for other task.
+/*
 exports.allMembersReports = function (req, res) {
     //Find all the reporters
     Reporter
@@ -35,4 +66,4 @@ exports.allProjects = function (req, res) {
                 });
             }
         });
-}
+}*/
